2b45a1567f290eeb99b21dc981e694759d3c8615
minsuwai <mswwai15@gmail.com>
2025-05-28 00:41:24 +0630

    setup and add post type

---
+++ event-manager.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: Event Manager
+ * Description: A custom plugin to manage and display events.
+ * Version: 1.0
+ * Author: Minsuwai
+ */
+
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
+
+// Include the main plugin file
+require_once plugin_dir_path(__FILE__) . 'includes/event-manager-init.php';
